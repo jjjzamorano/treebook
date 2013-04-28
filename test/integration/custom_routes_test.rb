a6451c2bef04d85bc692066a402ecc441ec4a929
Juan Zamorano <juan.zamorano@raremethod.com>
2013-04-28 16:18:44 -0600

    Added the profile controller, updated a profile show page & added scoped status to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -18,4 +18,8 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
 		assert_response :success
 	end
 
+	test "that a profile page works" do
+		get '/jjjzamorano'
+		assert_response :success
+	end
 end
